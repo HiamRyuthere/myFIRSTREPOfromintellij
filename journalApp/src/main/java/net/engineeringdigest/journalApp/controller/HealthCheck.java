f7f8f145ca1865baf32dfa001cfcd528a4b12797
Harsh <159010140+HiamRyuthere@users.noreply.github.com>
2024-09-11 22:42:23 +0530

    add first first.

--- journalApp/src/main/java/net/engineeringdigest/journalApp/controller/HealthCheck.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/controller/HealthCheck.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/Health")
 public class HealthCheck {
 
-   
+   //THIK HAI BHAIYA
 
     @GetMapping
     public String healthCheck(){
